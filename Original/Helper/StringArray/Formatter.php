a33747aca85061c0d3657c5215b9c98462377540
clamever <1095841676@qq.com>
2014-10-28 14:13:10 +0800

    新增格式化字符串的类和方法
    新增格式化字符串的类和方法

---
+++ Original/Helper/StringArray/Formatter.php
@@ -0,0 +1,24 @@
+<?php	namespace Original\Helper\StringArray;
+/**
+* 用于格式化某些特殊的字符串格式 Formatter.php
+*
+* @date: 2014-10-28 下午2:02:23
+* @author: 张啟明<1095841676@qq.com>
+* @version: 1.0
+* @copyright: 2014 yasin.com.cn
+*/
+class Formatter {
+	/**
+	* 用来将邮件地址格式化为header头中的标准地址格式，例如a<b@c.com>
+	*
+	* @date: 2014-10-28 下午2:07:53
+	* @param $hostName 主机名（上面的a）
+	* @param $address 邮件地址名（上面的b@c.com）
+	* @author: 张啟明<1095841676@qq.com>
+	* @return:
+	*/
+	public function formatSendEmailAddress($hostName,$address) {
+		$formattedAddress = $hostName . "<" . $address . ">";
+		return $formattedAddress;
+	}
+}
